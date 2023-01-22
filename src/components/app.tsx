8bf5d2479acb3d59573bec041620210aa9ebadd1
Justin <44349024+notjsouza@users.noreply.github.com>
2023-01-22 04:03:24 -0800

    Full stack project

---
+++ src/components/app.tsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+
+import Contest from "./contest";
+import ContestList from "./contest-list";
+import AddNewContest from "./add-new-contest";
+
+const App = ({ initialData }) => {
+  const [page, setPage] = useState<"contestList" | "contest">(
+    initialData.currentContest ? "contest" : "contestList",
+  );
+  const [currentContest, setCurrentContest] = useState<
+    object | undefined
+  >(initialData.currentContest);
+
+  useEffect(() => {
+    window.onpopstate = (event) => {
+      const newPage = event.state?.contestId
+        ? "contest"
+        : "contestList";
+      setPage(newPage);
+      setCurrentContest({ id: event.state?.contestId });
+    };
+  }, []);
+
+  const navigateToContest = (contestId) => {
+    window.history.pushState(
+      { contestId },
+      "",
+      `/contest/${contestId}`,
+    );
+    setPage("contest");
+    setCurrentContest({ id: contestId });
+  };
+
+  const navigateToContestList = () => {
+    window.history.pushState({}, "", "/");
+    setPage("contestList");
+    setCurrentContest(undefined);
+  };
+
+  const onNewContest = (newContest) => {
+    window.history.pushState(
+      { contestId: newContest.id },
+      "",
+      `/contest/${newContest.id}`,
+    );
+    setCurrentContest(newContest);
+    setPage("contest");    
+  };
+
+  const pageContent = () => {
+    
+    switch (page) {
+      case "contestList":
+        return (
+          <>
+            <ContestList
+              initialContests={initialData.contests}
+              onContestClick={navigateToContest}
+            />
+            <AddNewContest onSuccess={onNewContest} />
+          </>
+        );
+      case "contest":
+        return (
+          <Contest
+            initialContest={currentContest}
+            onContestListClick={navigateToContestList}
+          />
+        );
+    }
+  };
+
+  return <div className="container">{pageContent()}</div>;
+};
+
+export default App;
